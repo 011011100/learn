57b587b35e1fdf9ff1a066fb25333eca3c0ed3db
LZH <LZH01101110@163.com>
2025-03-25 14:18:34 +0800

    feat: 添加pinia与unocss

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import router from "./router/index.ts";
 import './css/style.css'
 import App from './App.vue'
+import 'virtual:uno.css'
+
+const pinia = createPinia()
 
 createApp(App)
     .use(router)
+    .use(pinia)
     .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
     plugins: [
+        UnoCss(),
         AutoImport({
             // 你可以指定要自动导入的库
             imports: [
                 'vue',
                 'vue-router',
+                'pinia',
                 // ...其他库
             ],
-            eslintrc: {
-                enabled: true,
-                filepath: './.eslintrc-auto-import.json',
-            }
         }),
         Components({
             resolvers: [
