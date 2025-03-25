480e80ac3fd70ea84a6616b9c4d2051089983195
LZH <LZH01101110@163.com>
2025-03-25 11:13:45 +0800

    feat: 添加项目配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        AutoImport({
+            // 你可以指定要自动导入的库
+            imports: [
+                'vue',
+                'vue-router',
+                // ...其他库
+            ],
+            eslintrc: {
+                enabled: true,
+                filepath: './.eslintrc-auto-import.json',
+            }
+        }),
+        Components({
+            resolvers: [
+                NaiveUiResolver(),
+            ],
+        }),
+        vue()
+    ],
 })
