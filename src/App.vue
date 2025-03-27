464d047741920d042dcabca313fa2886d1e1d825
LZH <LZH01101110@163.com>
2025-03-27 15:32:49 +0800

    feat: 主体包裹

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const theme = ref(null); // 你可以设置为 `null` 来使用默认主题
 </script>
 
 <template>
   <main>
-    <router-view />
+    <n-config-provider :theme="theme">
+      <router-view/>
+    </n-config-provider>
   </main>
 </template>
 
