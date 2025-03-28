4882e32433ceac753e2aa9c8a45da0ca372e8c04
LZH <LZH01101110@163.com>
2025-03-28 10:59:12 +0800

    feat: tsx配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import AutoImport from 'unplugin-auto-import/vite'
 import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import {defineConfig} from 'vite'
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
     plugins: [
         UnoCss(),
+        vueJsx(),
         AutoImport({
             // 你可以指定要自动导入的库
             imports: [
