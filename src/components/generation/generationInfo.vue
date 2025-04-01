cdb3f4df245bce1f0cf348361b83d72de87719a5
LZH <LZH01101110@163.com>
2025-04-01 17:30:57 +0800

    feat: 添加世代详情页面

---
+++ src/components/generation/generationInfo.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import {getGenerationsInfo} from "@/api/pokemon/generationApi.ts";
+
+const props = defineProps<{
+  index: number
+}>()
+
+const data = ref<any>()
+
+onMounted(() => {
+  getGenerationsInfo(props.index).then((res: any) => {
+    data.value = res.data
+  })
+})
+</script>
+
+<template>
+  {{ data }}
+</template>
+
+<style scoped>
+
+</style>
