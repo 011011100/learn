902f86b3f0a02069c825e5498444ce917ccd2fe6
LZH <LZH01101110@163.com>
2025-04-01 17:31:46 +0800

    feat: 添加跳转路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,18 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import home from '../components/index.vue'
 import berry from '../components/berry/index.vue'
 import berryInfo from '../components/berry/berryInfo.vue'
+import generationInfo from '../components/generation/generationInfo.vue'
 
 const routes = [
-    {path: '/', component: home, name:'home'},
-    {path: '/berry', component: berry, name: 'berry', },
-    {path: '/berryInfo/:item', component: berryInfo, name: 'berryInfo', props:true},
+    {path: '/', component: home, name: 'home'},
+    {path: '/berry', component: berry, name: 'berry',},
+    {path: '/berryInfo/:item', component: berryInfo, name: 'berryInfo', props: true},
+    {
+        path: '/generationInfo/:index',
+        component: generationInfo,
+        name: 'generationInfo',
+        props: (route: any) => ({index: Number(route.params.index)})
+    },
 ]
 
 const router = createRouter({
