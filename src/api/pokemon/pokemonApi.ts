b2c0fdd7de323aca049bbf992e626cb1eb75b374
LZH <LZH01101110@163.com>
2025-04-01 14:11:14 +0800

    feat: 添加宝可梦

--- src/api/pokemon/pokemonApi.ts
+++ src/api/pokemon/pokemonApi.ts
@@ -1,18 +1,38 @@
 import api from "@/util/reqApi.ts";
 
+
 export type getResourceListData = {
-    limit?:number,
-    offset?:number
+    limit?: number,
+    offset?: number
 }
 
-
-export function getResourceList(data:getResourceListData):Promise<any> {
+export function getResourceList(data: getResourceListData): Promise<any> {
     return api({
         url: "/",
         method: "get",
-        params:{
-            limit:data.limit,
-            offset:data.offset
+        params: {
+            limit: data.limit,
+            offset: data.offset
+        }
+    });
+}
+
+export function getPokemonInfo(id: string): Promise<any> {
+    return api({
+        url: "pokemon",
+        method: "get",
+        params: {
+            id: id
+        }
+    });
+}
+
+export function getPokemonSpeciesInfo(id: string): Promise<any> {
+    return api({
+        url: "pokemon-species",
+        method: "get",
+        params: {
+            id: id
         }
     });
 }
