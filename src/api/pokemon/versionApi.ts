8e2a9154f6eb86b81418f0ceae7bcb195471bc24
LZH <LZH01101110@163.com>
2025-04-02 17:29:35 +0800

    feat: 添加版本获取api

--- src/api/pokemon/versionApi.ts
+++ src/api/pokemon/versionApi.ts
@@ -17,6 +17,13 @@ export function getVersionList(data?: getVersionData) {
     });
 }
 
+export function getVersionInfo(id: number) {
+    return api({
+        url: `/version/${id}`,
+        method: "get",
+    });
+}
+
 export function getVersionGroupList(data?: getVersionData) {
     return api({
         url: "/version-group",
@@ -24,5 +31,12 @@ export function getVersionGroupList(data?: getVersionData) {
         params: {
             data
         }
+    });
+}
+
+export function getVersionGroupInfo(id: number) {
+    return api({
+        url: `/version-group/${id}`,
+        method: "get",
     });
 }
