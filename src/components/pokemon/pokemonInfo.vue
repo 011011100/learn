6a648955f58ff427234b5e3c0d26a265da9443ec
LZH <LZH01101110@163.com>
2025-04-08 17:45:03 +0800

    feat: 添加宝可梦详情页面

---
+++ src/components/pokemon/pokemonInfo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+interface Props {
+  id: string
+}
+
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+  {{props.id}}
+</template>
+
+<style scoped>
+
+</style>
