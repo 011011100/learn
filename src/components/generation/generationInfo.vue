b6d7b769cb79d70bfba64428d11b31edf87eaea4
LZH <LZH01101110@163.com>
2025-04-08 17:21:23 +0800

    feat: 调整渐变颜色

--- src/components/generation/generationInfo.vue
+++ src/components/generation/generationInfo.vue
@@ -90,8 +90,9 @@ function randomColor() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  const a = Math.random();
-  return `rgba(${r},${g},${b},${a})`;
+  // const a = Math.random();
+  // return `rgba(${r},${g},${b},${a})`;
+  return `rgb(${r},${g},${b})`;
 }
 
 </script>
